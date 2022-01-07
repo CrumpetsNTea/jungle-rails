2f0e06234cdc8767bd1772be1b6711177e70b41e
CrumpetsNTea <t.botell@gmail.com>
2022-01-06 19:35:36 -0500

    Add sales to db. Add sales page to admin

---
+++ app/models/sale.rb
@@ -0,0 +1,15 @@
+class Sale < ActiveRecord::Base
+# these methods are now available in our views
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
+
+end
