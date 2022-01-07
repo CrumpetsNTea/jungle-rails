e391538ee4a1897340c2b5bfae52d59c504316dc
CrumpetsNTea <t.botell@gmail.com>
2022-01-06 19:48:44 -0500

    Add Active sale info bar along top of page as per EDD video

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,6 +1,8 @@
 module SalesHelper
 
+  #pass in the result of the class method inside sale.rb
 def active_sale?
+  Sale.active.any?
 end
 
 
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,13 @@
 class Sale < ActiveRecord::Base
+
+  #Creating AR Scope - making it really easy to query sales
+  #checks to see if there are any active sales in the database uses
+  #placeholders '?' and then we provide the two dynamic values to the where clause after the comma as the two values we want
+def self.active
+  where("sales.starts_on <= ? AND sales.ends_on >= ?", 
+  Date.current, Date.current)
+end
+
 # these methods are now available in our views
   def finished?
     ends_on < Date.current
